5dff72474574166eb83f9055f27c3473202fbdd6
Joe Fredette <jfredett@gmail.com>
2011-10-11 21:28:02 -0400

    reorganize sortah/parser.rb

--- lib/sortah/parser.rb
+++ lib/sortah/parser.rb
@@ -4,19 +4,11 @@ module Sortah
   class Parser
     include Singleton
 
-    attr_reader :maildir
-
-    def maildir(maildir_path = nil)
-      @maildir = maildir_path if maildir_path
-      @maildir
-    end
-
-    def email
-      
-    end
+    ##object-level interaction
 
     def clear
       @destinations = Destinations.new
+      @lenses = {}
     end
 
     def initialize
@@ -31,6 +23,23 @@ module Sortah
       self.instance_eval &block
     end
 
+    # TODO: refactor the below to some modules which get
+    # mixed in -- Sortah::Language::{Config,Elements}
+
+    ## metadata/config data
+
+    attr_reader :maildir
+
+    def maildir(maildir_path = nil)
+      @maildir = maildir_path if maildir_path
+      @maildir
+    end
+
+    def email
+
+    end
+
+    ## language elements
     def destination(name, args)
       raise ParseErrorException if @destinations[name]
       @destinations[name] = args 
