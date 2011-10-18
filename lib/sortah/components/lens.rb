7e9a6f6e59f4e75cde5e8ac02042a8857ff0a15c
Joe Fredette <jfredett@gmail.com>
2011-10-18 07:44:34 -0400

    refactor lens to use component

--- lib/sortah/components/lens.rb
+++ lib/sortah/components/lens.rb
@@ -1,25 +1,21 @@
 require 'sortah/util/component_collection'
+require 'sortah/util/component'
 
 module Sortah
   class Lenses < ComponentCollection
   end
 
-  class Lens
-    attr_reader :name
-
-    def initialize(name, opts = {}, block)
-      @dependencies = opts[:lenses] || []
-      @provides_value = opts[:pass_through]
-      @name = name
-      @definition = block
+  class Lens < Component
+    def dependencies
+      @opts[:lenses] || []
     end
 
-    def defined?(context)
-      context.include?(@name)
+    def provides_value? 
+      !!@opts[:pass_through]
     end
 
     def valid?(context)
-      @dependencies.each do |lens|
+      dependencies.each do |lens|
         raise ParseErrorException unless context.include? lens
       end
     end
