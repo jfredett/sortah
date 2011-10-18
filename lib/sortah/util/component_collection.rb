6129849670513f8cf829f308743d28b07ffef420
Joe Fredette <jfredett@gmail.com>
2011-10-18 07:44:06 -0400

    wrap component_collection in the Sortah module

--- lib/sortah/util/component_collection.rb
+++ lib/sortah/util/component_collection.rb
@@ -1,15 +1,18 @@
-class ComponentCollection < Hash
-  def <<(component)
-    return unless component.respond_to? :name
-    raise Sortah::ParseErrorException if component.defined?(self)
-    self[component.name] = component
-  end
+module Sortah
+  class ComponentCollection < Hash
+    def <<(component)
+      return unless component.respond_to? :name
+      raise ParseErrorException if component.defined?(self)
+      self[component.name] = component
+    end
 
-  def valid?
-    return if self.empty?
-    self.each_value do |value|
-      next unless value.respond_to? :valid?
-      value.valid?(self)
+    def valid?
+      return if self.empty?
+      self.each_value do |value|
+        # someone might have registered a singleton method? 
+        next unless value.respond_to? :valid? 
+        value.valid?(self)
+      end
     end
   end
 end
