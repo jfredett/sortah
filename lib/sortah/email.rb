34137fdc664e2af1dc06ea5c0969b5ac0d718a34
Joe Fredette <jfredett@gmail.com>
2011-10-21 08:47:36 -0400

    Refactor email class.

--- lib/sortah/email.rb
+++ lib/sortah/email.rb
@@ -1,43 +1,44 @@
+require 'delegate'
+require 'mail'
 module Sortah
-  class Email 
+  class Email < DelegateClass(Mail)
     def self.wrap(context, metadata = {})
       Email.new(context, metadata)
     end
 
     def method_missing(meth, *args, &blk)
-      if base_respond_to?(meth)
-        super
-      elsif @mail.respond_to?(meth)
-        @mail.send(meth) #access only, no setting
-      elsif metadata.keys.include?(meth)
-        @metadata[meth] unless @metadata[meth] == :pass_through
-      end
-    end
-
-    alias_method :base_respond_to?, :respond_to?
-    def respond_to?(meth)
-      base_respond_to?(meth) || @mail.respond_to?(meth)
+      return @metadata[meth] if has_data_for?(meth)
+      super rescue nil
     end
 
     def process(lens)
       return unless @metadata[lens.name].nil?
       if lens.provides_value? 
-        @metadata[lens.name] = self.instance_eval &lens.block 
+        @metadata[lens.name] = run!(lens.block)
       else 
-        self.instance_eval &lens.block
+        run!(lens.block)
         @metadata[lens.name] = :pass_through
       end
     end
 
     private
 
+    def has_data_for?(meth)
+      @metadata.keys.include?(meth) and 
+      @metadata[meth] != :pass_through 
+    end
+
     def email
       self 
     end
 
+    def run!(block)
+      self.instance_eval &block
+    end
+
     def initialize(context, metadata)
-      @mail = context
       @metadata = metadata
+      super(context)
     end
 
   end
