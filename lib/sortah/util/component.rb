cbb3c71d470e89a1212afee0c3e09aa12c912ac6
Joe Fredette <jfredett@gmail.com>
2011-10-23 22:56:19 -0400

    reword the block retrieval code in the component class

--- lib/sortah/util/component.rb
+++ lib/sortah/util/component.rb
@@ -5,7 +5,7 @@ module Sortah
     def initialize(name, opts = {}, *potential_block) 
       @name = name
       @opts = opts
-      @block = potential_block.first if potential_block.size > 0
+      @block = potential_block.first unless potential_block.empty?
     end
 
     def run_dependencies!(email, context)
